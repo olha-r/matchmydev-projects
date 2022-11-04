40014bb8efa257e2472af766f016597a89b5155d
Raulet Olha <raulet.olha@gmail.com>
2022-11-04 16:17:11 +0100

    Add code into a form, change css of datepicker

--- front-vanilla/src/components/project-create/form.js
+++ front-vanilla/src/components/project-create/form.js
@@ -3,54 +3,57 @@ import { messages } from "../../messages.js"
 function renderForm() {
     const formContent = `
     <h1>${messages.createProject.h1}</h1>
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-12 col-md-9">
+        <div class="row justify-content-center align-items-center">
             <form>
-                <div class="mb-3">
-                    <label for="name" class="form-label required">${messages.createProject.form.name.label}</label>
-                    <input name="name" id="name" type="text" class="form-control focus-grey"
-                        aria-describedby="nameHelp">
-                    <div id="nameHelp" class="form-text">${messages.createProject.form.name.helpText}</div>
+                <div class="row">
+                    <div class="col-md-8 mb-3">
+                        <label for="name" class="form-label required">${messages.createProject.form.name.label}</label>
+                        <input name="name" id="name" type="text" class="form-control focus-grey"
+                            aria-describedby="nameHelp">
+                        <div id="nameHelp" class="form-text">${messages.createProject.form.name.helpText}</div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="code" class="form-label required">${messages.createProject.form.code.label}</label>
+                        <input name="code" id="code" type="text" class="form-control focus-grey"
+                            aria-describedby="codeHelp">
+                        <div id="codeHelp" class="form-text">${messages.createProject.form.code.helpText}</div>
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label required">${messages.createProject.form.description.label}</label>
-                    <textarea name="description" id="description" class="form-control focus-grey"
-                        rows="10"></textarea>
+                    <textarea name="description" id="description" class="form-control focus-grey" rows="10"></textarea>
                     <div id="descriptionHelp" class="form-text">${messages.createProject.form.description.helpText}</div>
                 </div>
-
                 <div class="row mb-3">
-                    <div class="col-md-6 col-12">
+                    <div class="col-md-6">
                         <label for="startDate" class="form-label required">${messages.createProject.form.startDate.label}</label>
                         <input name="startDate" type="date" id="startDate" class="form-control my-2 focus-grey"
                             min="2018-01-01" />
+                    </div>
+                    <div class="col-md-6">
                         <label for="endDate" class="form-label">${messages.createProject.form.endDate.label}</label>
                         <input name="endDate" type="date" id="endDate" class="form-control my-2 focus-grey"
                             min="2018-01-01" />
                         <div id="endDateHelp" class="form-text">${messages.createProject.form.endDate.helpText}</div>
                     </div>
-                    <div class="col">
-                        <div class="form-check form-switch">
-                            <div class="row">
-                                <label for="production" class="form-label status required">${messages.createProject.form.production.label}</label>
-                                <div class="col mx-5">
-                                    <input name="production" type="checkbox" id="production"
-                                        class="form-check-input mb-2 focus-grey" role="switch">
-                                </div>
-                            </div>
+                </div>
+                <div class="form-check form-switch mb-3">
+                    <div class="row">
+                        <label for="production" class="form-label status required">${messages.createProject.form.production.label}</label>
+                        <div class="col mx-5">
+                            <input name="production" type="checkbox" id="production"
+                                class="form-check-input mb-2 focus-grey" role="switch">
                         </div>
                     </div>
                 </div>
-
-                <div class="row">
+                <div class="row mb-3">
                     <div class="col d-flex justify-content-end content">
                         <button type="submit" class="btn col-12 col-md-2 btn-create">${messages.createProject.form.submit}</button>
                     </div>
                 </div>
             </form>
         </div>
-    </div>
-        `;
+    `;
 
     const main = document.getElementById("main");
     main.innerHTML = formContent;
