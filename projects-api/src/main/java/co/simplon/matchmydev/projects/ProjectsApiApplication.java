fdd722f7e97856eee31cf26d670586cd36b3c90a
manal benallal <manelbenallal@hotmail.fr>
2022-11-18 17:33:36 +0100

    initial commit

---
+++ projects-api/src/main/java/co/simplon/matchmydev/projects/ProjectsApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.matchmydev.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApiApplication.class, args);
+	}
+
+}
