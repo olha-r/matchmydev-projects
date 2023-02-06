d0a8ed9b67f0f59e2def4a74d39d389b3a974a8a
manal benallal <manelbenallal@hotmail.fr>
2023-02-06 16:40:04 +0100

    add new file configuration

---
+++ projects-api/src/main/java/co/simplon/matchmydev/projects/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package co.simplon.matchmydev.projects.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173")
+		.allowedMethods("POST", "GET", "PATCH", "DELETE", "PUT");
+		
+	}
+}
+
+
