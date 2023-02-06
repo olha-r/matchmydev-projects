fa7f1c6cf9835ac58af26057bd667a980575444b
Raulet Olha <raulet.olha@gmail.com>
2023-02-06 20:11:09 +0100

    add project service impl

--- projects-api/src/main/java/co/simplon/matchmydev/projects/services/ProjectServiceImpl.java
+++ projects-api/src/main/java/co/simplon/matchmydev/projects/services/ProjectServiceImpl.java
@@ -1,5 +1,31 @@
 package co.simplon.matchmydev.projects.services;
 
-public class ProjectServiceImpl {
+import org.springframework.stereotype.Service;
+
+import co.simplon.matchmydev.projects.dtos.ProjectCreateDto;
+import co.simplon.matchmydev.projects.entities.Project;
+import co.simplon.matchmydev.projects.repositories.ProjectRepository;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepo;
+
+    public ProjectServiceImpl(ProjectRepository projectRepo) {
+	this.projectRepo = projectRepo;
+    }
+
+    @Override
+    public void createProject(ProjectCreateDto inputs) {
+	Project project = new Project();
+	project.setName(inputs.getName());
+	project.setCode(inputs.getCode());
+	project.setDescription(inputs.getDescription());
+	project.setStartDate(inputs.getStartDate());
+	project.setEndDate(inputs.getEndDate());
+	project.setProduction(inputs.isProduction());
+
+	this.projectRepo.save(project);
+    }
 
 }
