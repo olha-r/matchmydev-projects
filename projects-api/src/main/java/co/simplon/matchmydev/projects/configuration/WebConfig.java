806f8120151baa8e7f1753b19ea6c65fc3aa3707
Ewa Kadziolka <kalistaewa@gmail.com>
2023-02-07 17:27:22 +0100

    fix error of localhost

--- projects-api/src/main/java/co/simplon/matchmydev/projects/configuration/WebConfig.java
+++ projects-api/src/main/java/co/simplon/matchmydev/projects/configuration/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:5153", "http://127.0.0.1:5173")
+		.allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
 		.allowedMethods("POST", "GET", "PATCH", "DELETE", "PUT");
 		
 	}
