111304bd62cedce541af37981ea3b67c9aba972a
Nibal ABAZLI <nibalabazli@nibals-macbook-pro.home>
2023-02-07 17:54:35 +0100

    fix merge

--- projects-api/src/main/java/co/simplon/matchmydev/projects/configuration/WebConfig.java
+++ projects-api/src/main/java/co/simplon/matchmydev/projects/configuration/WebConfig.java
@@ -4,19 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		
-		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:5153", "http://127.0.0.1:5173")
-		.allowedMethods("POST", "GET", "PATCH", "DELETE", "PUT");
-		
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+	registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("POST",
+		"GET", "PATCH", "DELETE", "PUT");
+
+    }
 }
-
-
