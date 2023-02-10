b28355a0f748913c15909a5722901588684b0233
Raulet Olha <raulet.olha@gmail.com>
2023-02-10 10:18:59 +0100

    Create axiox plugin

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,20 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
+
 import './assets/main.css'
+import axiosPlugin from './plugins/axios'
 
 
 const app = createApp(App)
 
-const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL
-
-  });
-
-app.config.globalProperties.$axios = axiosInstance;
+app.use(axiosPlugin)
 
 app.use(router)
 
 app.mount('#app')
-
